4314e55a183007197df401e8530980c56b154978
Jordan de Sousa Cassiano <jordan@emporioht.com.br>
2015-04-10 21:00:20 -0300

    Mozaico
    Puglin para gerar galeria em grid

--- js/mozaico.js
+++ js/mozaico.js
@@ -3,18 +3,13 @@
 
     	var padrao = {
     		imagens : "",
-    		colunas : 6,
-    		caption : "Imagens"
+    		colunas : 4,
+    		caption : "Imagens",
+    		paginacao: true
     	}
 
     	var elemento = $(this),
-    		config   = $.extend( {}, padrao, opcoes );
-
-    		elemento.append( _geraLinhas( config.imagens, config.colunas ) );
-    		elemento.append( _geraRodape("", config.colunas ) );
-
-    		
-        
+    		config   = $.extend( {}, padrao, opcoes );         
     		_geraMozaicoEstrutura( elemento, config );
     }; 
 
@@ -28,11 +23,22 @@
     		if( config.caption )
     			divEstrutura.append( _geraMozaicoHeader( config.caption ) );
 
-    		var mozaicoCorpo = _geraMozaicoCorpo();
-    		
-			divEstrutura.append( mozaicoCorpo );
-			
-    		elementoAlvo.appendTo( mozaicoCorpo );
+    			elementoAlvo.append( _geraLinhas( config.imagens, config.colunas ) );		
+
+    		var mozaicoCorpo = _geraMozaicoCorpo();			    
+			    divEstrutura.append( mozaicoCorpo );
+    		    elementoAlvo.appendTo( mozaicoCorpo );
+    		    divEstrutura.append( _geraRodape( config , config.colunas ) );
+
+    		    _bindEvento( $(".mozaico-triger"), 'click', function( e ){
+    			        e.preventDefault();
+	         			$(".mozaico-triger").removeClass('selecionado')
+                        $(this).addClass('selecionado')
+		    			var img          = $( this).find('img'),    				    				
+				       	    theImage     = new Image();
+		                    theImage.src = img.prop('src');
+    			            _visualizacao( img.prop('src'), theImage.width, theImage.height );
+    		    });
     }
 
     function _geraMozaicoHeader( titulo )
@@ -48,7 +54,7 @@
     function _geraLinhas( dados, colunas )
     {
     	var mascaraDalinha   = "<tr>{{coluna}}</tr>",
-    		mascaraDacelula  = "<td><img width='128' height='128' src='{{imagem}}'></td>",
+    		mascaraDacelula  = "<td><a href='{{imagem}}' class='mozaico-triger'><img width='128' height='128' src='{{imagem}}'></a></td>",
     		html             = "",
     		htmlCelulas      = "",    	
     		imgAtual         = 0,	
@@ -61,7 +67,7 @@
     			for( var c = 0; c < colunas; c++ )
     			{
     				if( imgAtual < totalRegistros )
-    					htmlCelulas += mascaraDacelula.replace("{{imagem}}", dados[imgAtual] );
+    					htmlCelulas += mascaraDacelula.replace(/{{imagem}}/g, dados[imgAtual] );
 
     				imgAtual++;
     			}
@@ -71,9 +77,88 @@
     	return html;	
     }
 
-    function _geraRodape( controles, tamanho )
+    function _geraMozaicoPaginacao( qtdPaginas, qtdDados )
     {
-    	return "<tfoot><tr><td colspan='"+tamanho+"'></td></tr></tfoot>";
+    	var html = "<ul class='mozaico-paginacao'>";
+                    
+                for( var i = 1 ; i <= qtdPaginas; i++ )
+                	html += "<li><a href=''>"+i+"</a></li>";
+
+    		html += "</ul>";
+    	return html;
+    }
+
+    function _geraRodape( config, tamanho )
+    {
+    	var controles = "";
+    	if( config.paginacao == true )
+    	{
+    		controles += _geraMozaicoPaginacao( 2, 0);
+    	}
+    	return "<div class='mozaico-rodape' >"+ controles+"</div>";
+    }
+
+    function _visualizacao( srcImagem, largura, altura )
+    {
+    	if( $('#mascara')[0] == undefined )
+    	{
+    		var mascara = $("<div></div>", { id: "mascara", class: "mozaico-mascara" } );
+    			mascara.append( '<div class="mozaico-view"></div>' )
+    					  $("body").append( mascara )
+    	}
+
+    	var larguraWindow     = $(document).width(),
+    	    alturaWindow      = $(document).height(),
+    	    larguraContainer  = ( largura > larguraWindow ?  ( larguraWindow - 100 ) : largura  ),
+    		alturaContainer   = ( altura >  alturaWindow ? ( alturaWindow - 80 ) : altura ),
+    		topContainer      = ( altura >  alturaWindow ? '2%' : ( (alturaWindow/2) - (alturaContainer/2) )  );
+
+    	$(".mozaico-view")
+    		.html( '<span id="esquerdo"><<</span><img src="'+srcImagem+'" /><span id="direito" >>></span>' )
+    		.animate({  width  : larguraContainer , 
+    				    height : alturaContainer,
+    				    margin : topContainer +" auto" }, 'slow');
+
+    	$('#mascara').fadeIn();  
+
+    	_bindEvento( $("#esquerdo"), 'click', function(e){  e.preventDefault(); _anterior()   }); 
+    	_bindEvento( $("#direito"), 'click', function(e){  e.preventDefault();  _proxima()  });     	    	
+    }
+
+    function _proxima()
+    {
+    	var indiceClick = 0,
+    		elementos   = $(".mozaico-triger");
+
+    		elementos.each(function(indice, item){
+
+    			if( $(item).hasClass('selecionado') )
+    			{
+    				indiceClick = indice;
+    			}
+    		});
+
+    		if( elementos.length == ( indiceClick + 1 ))
+    			indiceClick = 0;
+    		else
+    			indiceClick += 1;
+
+    	var click = $(".mozaico-triger")[indiceClick];
+    		$(click).click();
+
+    		console.log( click, indiceClick )
+
+    }
+
+    function _anterior()
+    {
+    	$(".mozaico-triger").find('.selecionado').prev().find('a').click();
+    }
+
+
+    function _bindEvento( elemento, evento, retorno )
+    {
+    	elemento.on( evento, retorno );
     }
 
 })( jQuery );
